e5704452aa9813ba37fd7e94d5842c7f0c396599
greatgy <38930578+greatgy@users.noreply.github.com>
2019-03-21 10:03:31 +0800

    Create NumberOf1.java

---
+++ 算法/NumberOf1.java
@@ -0,0 +1,20 @@
+public class NumberOf1 {
+		
+	//n&(n-1)  可以用来判断整数中1的个数，及是否是2的幂次数
+	public static void main(String[] args){
+		int reult = solution(15);
+		System.out.println(reult);
+	}
+	public static int solution(int n) {
+        int count = 0; 
+        while(n != 0){
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+    }
+	
+	public static boolean is2Power(int n){
+		return n> 0 && (n &(n-1)) == 0;
+	}
+}
